1adc20349aa35a97a37b6bf867003b439a0bbb22
sdubrez <22425976+imsamdez@users.noreply.github.com>
2022-12-27 21:09:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const bouncy = require("bouncy");
+
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const ports = [4100, 4101, 4102, 4103, 4104];
+let i = 0;
+
+var server = bouncy(function (req, res, bounce) {
+  const port = ports[between(0, 4)];
+
+  console.log("request " + i + " sending to -> ", port);
+  i++;
+  bounce(port);
+});
+
+server.listen(4000);
